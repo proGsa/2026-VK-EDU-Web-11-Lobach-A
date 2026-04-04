91f2b27734614ec8ac08ca98508f7ff651b39cd4
Nastya Lobach <nastya@lobach.info>
2026-04-04 18:04:19 +0300

    Внесла правки

--- public/run.py
+++ public/run.py
@@ -38,7 +38,16 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='ask.html')),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('ask/', TemplateView.as_view(template_name='ask.html'), name='ask'),
+    path(
+        'question/<int:question_id>/',
+        TemplateView.as_view(template_name='question.html'),
+        name='question',
+    ),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('signup/', TemplateView.as_view(template_name='signup.html'), name='signup'),
+    path('profile/', TemplateView.as_view(template_name='profile.html'), name='profile'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=BASE_DIR / 'static')
 # --------------------------
